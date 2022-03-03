60c14a70ce4e98c3fa85e6ce372d652419e525f7
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-03-04 00:59:06 +0530

    reduced logo width

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PlayGround from "./components/PlayGround/PlayGround";
 export default function App() {
   return (
     <Fragment>
-      <img alt="logo" src="fam-logo.gif" width={"200px"} />
+      <img alt="logo" src="fam-logo.gif" width={"150px"} />
       <PlayGround />
     </Fragment>
   );
