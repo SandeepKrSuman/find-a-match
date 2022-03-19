e1978b9850432857d454b7c850c3dbbd51c8dd54
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-03-19 14:06:16 +0530

    console log onload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import BrandBar from "./components/BrandBar/BrandBar";
 import PlayGround from "./components/PlayGround/PlayGround";
 
 export default function App() {
+  useEffect(() => {
+    console.log(`    ╔═══╗        ╔╗    ╔═══╗    ╔═╗╔═╗      ╔╗     ╔╗  
+    ║╔══╝        ║║    ║╔═╗║    ║║╚╝║║     ╔╝╚╗    ║║  
+    ║╚══╗╔╗╔═╗ ╔═╝║    ║║ ║║    ║╔╗╔╗║╔══╗ ╚╗╔╝╔══╗║╚═╗
+    ║╔══╝╠╣║╔╗╗║╔╗║    ║╚═╝║    ║║║║║║╚ ╗║  ║║ ║╔═╝║╔╗║
+   ╔╝╚╗  ║║║║║║║╚╝║    ║╔═╗║    ║║║║║║║╚╝╚╗ ║╚╗║╚═╗║║║║
+   ╚══╝  ╚╝╚╝╚╝╚══╝    ╚╝ ╚╝    ╚╝╚╝╚╝╚═══╝ ╚═╝╚══╝╚╝╚╝
+                                                       
+                                                       
+`);
+    console.log("Made with ❤ by SandeepKrSuman");
+    console.log("https://github.com/SandeepKrSuman");
+  }, []);
   return (
     <Fragment>
       <BrandBar />
