ac49bd6847ec5144af8fa3be095c76823aa5d8a7
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-03-19 13:23:48 +0530

    get from searchParams

--- src/components/WelcomeUser/WelcomeUser.js
+++ src/components/WelcomeUser/WelcomeUser.js
@@ -1,20 +1,29 @@
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import "./WelcomeUser.css";
 
 export default function WelcomeUser() {
+  const [searchParams] = useSearchParams();
+  const from =
+    searchParams.get("from") && searchParams.get("from").length > 0
+      ? searchParams.get("from")
+      : "Your Bestie";
+  const moves =
+    searchParams.get("y") && parseInt(searchParams.get("y")) > 6
+      ? parseInt(searchParams.get("y"))
+      : "minimum number of";
   return (
     <div className="main-container">
       <Typography className="typ" variant="h4" component="div" gutterBottom>
-        xyz
+        {`${from}`}
       </Typography>
       <Typography className="typ" variant="h6" component="div" gutterBottom>
         Challenged you to find all the matches in
       </Typography>
       <Typography className="typ" variant="h6" component="div" gutterBottom>
-        10 moves.
+        {`${moves} moves.`}
       </Typography>
       <br /> <br />
       <Link className="accept-link" to="/">
