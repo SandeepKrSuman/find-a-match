70b6e5bfdb398fbd082ebdc288bb2e1f0283b3bc
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2023-02-08 17:16:21 +0530

    optimized card design

--- src/components/Cards/EmojiCard.js
+++ src/components/Cards/EmojiCard.js
@@ -1,33 +1,36 @@
-import { Fragment } from "react";
 import Grid from "@mui/material/Grid";
 import "./EmojiCard.css";
 
 export default function EmojiCard(props) {
   return (
-    <Fragment>
-      <Grid className="emoji-container" item xs={4} md={3}>
-        <div
-          className={`${props.show ? "card is-flipped do-not-click" : "card"}`}
-          onClick={() => {
-            props.execute();
-          }}
-        >
-          <div className="card__face card__face--front">
-            <img
-              className="emoji emoji-front"
-              alt="emoji"
-              src="/assets/logo-grey.png"
-            />
-          </div>
-          <div className="card__face card__face--back">
-            <img
-              className="emoji"
-              alt="emoji"
-              src={`/assets/emoji-${props.imgId}.png`}
-            />
-          </div>
+    <Grid
+      className="emoji-container"
+      sx={{ padding: "15px" }}
+      item
+      xs={4}
+      md={3}
+    >
+      <div
+        className={`${props.show ? "card is-flipped do-not-click" : "card"}`}
+        onClick={() => {
+          props.execute();
+        }}
+      >
+        <div className="card__face card__face--front">
+          <img
+            className="emoji emoji-front"
+            alt="emoji"
+            src="/assets/logo-grey.png"
+          />
         </div>
-      </Grid>
-    </Fragment>
+        <div className="card__face card__face--back">
+          <img
+            className="emoji"
+            alt="emoji"
+            src={`/assets/emoji-${props.imgId}.png`}
+          />
+        </div>
+      </div>
+    </Grid>
   );
 }
